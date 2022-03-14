569ab439449f7b8090f779738c33382b23f039ca
Nika Ptushkina <nika.ptushkina@gmail.com>
2022-03-13 18:23:21 -0700

    Light code reorganization

--- express_server.js
+++ express_server.js
@@ -1,44 +1,33 @@
-// requirements
+// imports & requirements
 const express = require("express");
+const bodyParser = require("body-parser");
+const req = require("express/lib/request");
+
+// set-up server
 const app = express();
 const PORT = 8080; // default port 8080
-const bodyParser = require("body-parser");
-
-// convert request body into string (from Buffer)
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true})); // convert request body into string (from Buffer)
 
 // set ejs as view engine
 app.set("view engine", "ejs");
-
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
-
-// GET route for /urls
-app.get("/urls", (req,res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`)
 });
 
-// POST route to receive form submissions
-app.post("/urls", (req,res) => {
-  console.log(req.body) // Log the POST request body to the console
-  res.send("Ok");       // Respond with "Ok"
+// databases
+const urlDatabase = {
+  b2xVn2: { longURL: "http://www.lighthouselabs.ca" },
+  s9m5xK: { longURL: "http://www.google.com" }
+};
+
+// GET REQUESTS
+// send HTML
+app.get("/hello", (req,res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/", (req,res) => {
   res.send("Hello!");
-});
-
-// JSON string representing the entire urlDatabase object
-app.get("/urls.json", (req,res) => {
-  res.json(urlDatabase);
-});
-
-// send HTML
-app.get("/hello", (req,res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 // GET Route to Show the Form
@@ -46,13 +35,27 @@ app.get("/urls/new", (req,res) => {
   res.render("urls_new");
 });
 
+// JSON string representing the entire urlDatabase object
+app.get("/urls.json", (req,res) => {
+  res.json(urlDatabase);
+});
+
+// Route for /urls
+app.get("/urls", (req,res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`)
+// POST REQUESTS
+// POST route to receive form submissions
+app.post("/urls", (req,res) => {
+  console.log(req.body) // Log the POST request body to the console
+  res.send("Ok");       // Respond with "Ok"
 });
 
 // returns 6 random alphanumeric characters
