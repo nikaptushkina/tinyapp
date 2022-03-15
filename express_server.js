5697c6560ebf35dd95f5aacb709f05b89c363a84
Nika Ptushkina <nika.ptushkina@gmail.com>
2022-03-15 05:24:56 -0700

    Add GET /login endpoint

--- express_server.js
+++ express_server.js
@@ -87,6 +87,14 @@ app.get("/register", (req,res) => {
   res.render("urls_register", templateVars);
 });
 
+// login page
+app.get("/login", (req,res) => {
+  const templateVars = {
+    user: req.cookies["user_id"]
+  };
+  res.render("urls_login", templateVars)
+});
+
 // POST REQUESTS
 // POST route to receive form submission
 app.post("/urls", (req,res) => {
