55b6ca0e9139e8d7fe5588ab80a7523c6cf94592
Nika Ptushkina <nika.ptushkina@gmail.com>
2022-03-15 05:14:31 -0700

    Create functions to check if blank and to check if user is already in database

---
+++ helperFunctions.js
@@ -0,0 +1,33 @@
+// returns 6 random alphanumeric characters
+function generateRandomString() {
+  let result = '';
+  let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 6; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+};
+
+// checks if email of password are blank
+const checkBlank =  function(req) {
+  let results = false;
+  if (req.body.password === '' || req.body.password === '') {
+    results = true;
+  }
+  return results;
+};
+
+// checks if user's email is in the database and is able to return ID
+const checkEmail = function(email, data) {
+  for (const user in data) {
+    if (email === data[user].email) {
+      return user;
+    }
+  }
+};
+
+module.exports = {
+  generateRandomString,
+  checkBlank,
+  checkEmail
+}
